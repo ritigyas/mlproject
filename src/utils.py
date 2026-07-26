635193a784456cf5f0fdd1da9e5716b187ce8154
ritigyas <rsingh7_be23@thapar.edu>
2026-07-26 16:44:00 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,8 @@ import os
 import sys
 import dill
 from src.exceptions import CustomException
-
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path: str, obj: object):
     try:
         dir_path = os.path.dirname(file_path)
@@ -13,3 +14,42 @@ def save_object(file_path: str, obj: object):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+
+            para = param.get(model_name, {})
+
+            gs = GridSearchCV(
+                estimator=model,
+                param_grid=para,
+                cv=3,
+                n_jobs=-1
+            )
+
+            gs.fit(X_train, y_train)
+
+            best_model = gs.best_estimator_
+
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
+
+            report[model_name] = r2_score(y_test, y_test_pred)
+
+            # Update the trained model back into the dictionary
+            models[model_name] = best_model
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
